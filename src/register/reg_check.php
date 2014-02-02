7ac6c450b08fdb20055eb9c0cf599cfcab6fd42b
Daniel Watts <silwly@gmail.com>
2014-02-02 19:47:13 +0000

    added password security
    For this moment in time, sha-1 is the password hashing algorithm that
    will be used. In the near future, I will change this to the bcrypt
    algorithm.

--- src/register/reg_check.php
+++ src/register/reg_check.php
@@ -2,21 +2,28 @@
 include('../inc/p-connect.php');
 
 if(isset($_POST['submit'])){
+	//grab the users input
 	$username = $_POST['username'];
-	$username_safe = $mysqli->real_escape_string($username);
 	$password = $_POST['password'];
-	$password_safe = $mysqli->real_escape_string($password);
 	$password2 = $_POST['password2'];
-	$password2_safe = $mysqli->real_escape_string($password2);
 	$email = $_POST['email'];
 	$email_safe = $mysqli->real_escape_string($email);
 
-	if($password_safe == $password2_safe){
-			$sql = "INSERT INTO users (username,password,email) VALUES ('$username_safe', '$password_safe', '$email_safe')";
-			$mysqli->query($sql);
-			echo('Your new account awaits you!');
-	} else {
+	//santatise the input
+	$username_safe = $mysqli->real_escape_string($username);
+	$password_safe = $mysqli->real_escape_string($password);
+	$password2_safe = $mysqli->real_escape_string($password2);
+	$email_safe = $mysqli->real_escape_string($email);
+
+	if($password_safe != $password2_safe){
 		die('Fatal erorr: Your password\'s don\'t match, please go back and fix this.');
+	} else {
+		//create an md5 hash of the password
+		$password_safe = sha1($password_safe);
+		//insert into db
+		$sql = "INSERT INTO users (username,password,email) VALUES ('$username_safe', '$password_safe', '$email_safe')";
+		$mysqli->query($sql);
+		echo('Your new account awaits you!');
 	}
 }
 ?>
