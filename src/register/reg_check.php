4986577786fbbadf732390b5641a9859db86f5e7
Daniel Watts <silwly@gmail.com>
2014-02-02 23:37:39 +0000

    Stop registration
    If a user tries to register with account information already stored on
    the database, the user has failed.
    If the user tries to enter no information into the fields, the user has
    failed.
    If the user enters un-matching passwords, the user has failed.

--- src/register/reg_check.php
+++ src/register/reg_check.php
@@ -1,42 +1,78 @@
 <?php
 include('../inc/p-connect.php');
+$error_msg = "";
+//grab user input from form
+$username = $_POST['username'];
+$email = $_POST['email'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$password = $_POST['password'];
+$reppassword = $_POST['reppassword'];
 
+//blowfish information
+$blowfish_pre = '$2a$12$';
+$blowfish_end = '$';
+
+//blowfish accepts these characters as salts
+$Allowed_Chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./';
+$Chars_Len = 63;
+
+//18 would be secure as well
+$Salt_Length = 21;
+
+//set date
+$mysqli_date = date( 'y-m-d' );
+$salt = "";
+
+//create random string for salt
+for($i=0; $i<$Salt_Length; $i++)
+{
+	$salt .= $Allowed_Chars[mt_rand(0,$Chars_Len)];
+}
+
+//join together
+$bcrypt_salt = $blowfish_pre . $salt . $blowfish_end;
+
+//lets get some checks up in here
+
+$hashed_password = crypt($password, $bcrypt_salt);
 if(isset($_POST['submit'])){
-	$username = $_POST['username'];
-	$email = $_POST['email'];
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
-	$password = $_POST['password'];
-	$reppassword = $_POST['reppassword'];
-
-    //blowfish information
-    $blowfish_pre = '$2a$12$';
-    $blowfish_end = '$';
-
-	//blowfish accepts these characters as salts
-	$Allowed_Chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./';
-	$Chars_Len = 63;
-
-	//18 would be secure as well
-	$Salt_Length = 21;
-
-	$mysqli_date = date( 'y-m-d' );
-	$salt = "";
-
-	for($i=0; $i<$Salt_Length; $i++)
-	{
-    	$salt .= $Allowed_Chars[mt_rand(0,$Chars_Len)];
+	if(empty($_POST['username']) || empty($_POST['password']) || empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['email']) || empty($_POST['reppassword'])){
+		$error_msg = die('Please enter all fields required');
 	}
 
-	$bcrypt_salt = $blowfish_pre . $salt . $blowfish_end;
+	if($_POST['password'] != $_POST['reppassword']){
+		$error_msg = die('Your passwords don\'t match!');
+	}
 
-	if($password != $reppassword){
-		die('Ooops, looks like you\'ve spilt your coffee everywhere!<br />Not to worry, just head back and <b>make sure both your passwords match - otherwise... you\'ll just end up back here!');
-	} else {
+	$prep_stmt = "SELECT username FROM users WHERE username = ? LIMIT 1";
+	$stmt = $mysqli->prepare($prep_stmt);
 
-	$hashed_password = crypt($password, $bcrypt_salt);
+	if($stmt){
+		$stmt->bind_param('s', $username);
+		$stmt->execute();
+		$stmt->store_result();
 
-	$sql = "INSERT INTO users (email, reg_date, fname, lname, salt, password) VALUES ('$email', '$mysqli_date', '$fname', '$lname', '$salt', '$hashed_password')";
+		if($stmt->num_rows == 1){
+			$error_msg = die('Somebody has already registered with that username');
+		}
+	}
+
+	$prep_stmt = "SELECT email FROM users WHERE email = ?";
+	$stmt = $mysqli->prepare($prep_stmt);
+
+	if($stmt){
+		$stmt->bind_param('s', $email);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if($stmt->num_rows == 1){
+			$error_msg = die('Somebody has already registered with that email');
+		}
+	}
+
+	if(empty($error_msg)){
+	$sql = "INSERT INTO users (username, email, reg_date, fname, lname, salt, password) VALUES ('$username', '$email', '$mysqli_date', '$fname', '$lname', '$salt', '$hashed_password')";
 	$mysqli->query($sql);
 	echo 'Thanks for registering ' . $username . '.';
 	}
