1395ee7be22b54a7f267a513931e97fb71ea90ba
Daniel Watts <silwly@gmail.com>
2014-02-02 22:31:37 +0000

    changes
    +added all new fields
    -all old fields

--- src/register/index.php
+++ src/register/index.php
@@ -9,10 +9,12 @@
 </head>
 <body>
 	<form action="reg_check.php" method="post">
-		<input type="text" name="username" id="username" max-length="30" placeholder="username">
-		<input type="password" name="password" id="password" max-length="30" placeholder="password">
-		<input type="password" name="password2" id="password2" max-length="30" placeholder="confirm password">
-		<input type="email" name="email" id="email" max-length="30" placeholder="email">
+		<input type="username" name="username" id="username" placeholder="Enter your username">
+		<input type="email" name="email" id="email" placeholder="Enter your email">
+		<input type="text" name="fname" id="fname" placeholder="Enter your first name">
+		<input type="text" name="lname" id="lame" placeholder="Enter your last name">
+		<input type="password" name="password" id="password" placeholder="Enter your password">
+		<input type="password" name="reppassword" id="reppassword" placeholder="Repeat chosen password">
 		<button type="submit" name="submit" value="val">Register a new account</button>
 	</form>
 </body>
